9b96ded4e600e882202c3fa5e72b717c294eb89d
Luis Ramirez <luis@tibbo.com>
2023-04-19 03:18:59 +0000

    Adding libraries to project compilation.

--- ntios/run.sh
+++ ntios/run.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 NTIOS_LIB_PATH=$DIR/libs
 
-LD_LIBRARY_PATH=/usr/lib/arm-linux-gnueabihf/:$NTIOS_LIB_PATH
+LD_LIBRARY_PATH=$NTIOS_LIB_PATH:/usr/lib/arm-linux-gnueabihf/
 export LD_LIBRARY_PATH 
