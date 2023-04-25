8b31da9b13ea71510761ea9cc18da7875de5bdeb
Luis Ramirez <luis@tibbo.com>
2023-04-25 09:17:52 +0000

    Added tbt29

--- inc/global.h
+++ inc/global.h
@@ -1,5 +1,5 @@
-#ifndef __APP_GLOBAL_TBH__
-#define __APP_GLOBAL_TBH__
+#ifndef __APP_GLOBAL_H__
+#define __APP_GLOBAL_H__
 
 #define SSI_MAX_SIGNATURE_LEN 16
 
@@ -8,6 +8,7 @@
 #include "super_spi/super_spi.h"
 #include "tibbits/tbt13/tbt13.h"
 #include "tibbits/tbt28/tbt28.h"
+#include "tibbits/tbt29/tbt29.h"
 #include "tibbits/tbt30/tbt30.h"
 #include "tibbits/tbt42/tbt42.h"
 
